428d77ee5a47bb61df4b311ae0454b6d738e52eb
Jordan Dashel <jdashel@nypublicradio.org>
2019-10-25 11:19:46 -0400

    delete old comments

--- whatsonms/dynamodb.py
+++ whatsonms/dynamodb.py
@@ -62,10 +62,6 @@ class MetadataDB(DB):
             ProjectionExpression=self.metadata_key
         )
         return metadata if metadata else {}
-        # try:
-        #     return json.loads(metadata['Item']['metadata']) if metadata else {}
-        # except KeyError:
-        #     return {}
 
     def set_metadata(self, stream: str, metadata: Dict) -> Dict:
         """
