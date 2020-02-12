60aa328528bcc34e828745fa443df501b5111835
Jordan Dashel <jdashel@nypublicradio.org>
2020-02-12 18:07:09 -0500

    func tests 2

--- functests/end_to_end.py
+++ functests/end_to_end.py
@@ -9,38 +9,97 @@ import func_test_helpers as helpers
 
 stream = 'thefunk'
 
-nexgendata_raw = test_data.nexgen_sample()
-nexgendata_encoded = urlencode.quote(nexgendata_raw)
-nexgendata = dict(dict(xmltodict.parse(nexgendata_raw))['audio'])
+# TODO: readjust input index
+nexgen_inputs = [
+    test_data.nexgen_sample(),
+    test_data.nexgen_sample_2()
+]
 
-nexgendata_raw_2 = test_data.nexgen_sample_2()
-nexgendata_encoded_2 = urlencode.quote(nexgendata_raw_2)
-nexgendata_2 = dict(dict(xmltodict.parse(nexgendata_raw_2))['audio'])
+david_inputs = [
+    test_data.david_sample()
+]
 
-# woms song swan song
-woms_song_0 = f"https://api.demo.nypr.digital/whats-on/v1/update?stream={stream}&xml_contents={nexgendata_encoded}"
-woms_song_1 = f"https://api.demo.nypr.digital/whats-on/v1/update?stream={stream}&xml_contents={nexgendata_encoded_2}"
 woms_whatson = f"https://api.demo.nypr.digital/whats-on/v1/whats-on?stream={stream}"
 
-print("TESTING WOMS & NEXGEN\n")
-print("\tsending new track to woms ...", end=' ')
-r = requests.get(woms_update)
-print(f"{r.status_code}")
-assert r.status_code == 200
+def send_track(nexgen_inputs):
+    for nexgen_input in nexgen_inputs:
 
-print("\n\tasserting WOMs knows what's on ...", end=' ')
-r = requests.get(woms_whatson)
-print(f"{r.status_code}")
-assert r.status_code == 200
-whatson = ast.literal_eval(r.text)['data']['attributes']
-helpers.assert_and_report("title", whatson['title'], nexgendata['title'])
-helpers.assert_and_report("composer", whatson['mm_composer1'], nexgendata['comment1'])
-helpers.assert_and_report("ID", whatson['mm_uid'], nexgendata['number'])
+        nexgendata_encoded = urlencode.quote(nexgen_input)
+        nexgendata = dict(dict(xmltodict.parse(nexgen_input))['audio'])
+        woms_update = f"https://api.demo.nypr.digital/whats-on/v1/update?stream={stream}&xml_contents={nexgendata_encoded}"
 
-print("\n\ttesting that woms has playlist history")
+        print("\tsending new track to woms ...", end=' ')
 
+        r = requests.get(woms_update)
+        print(f"{r.status_code}\n")
+        assert r.status_code == 200
+
+        print("\tasserting WOMs knows what's on ...", end=' ')
+
+        r = requests.get(woms_whatson)
+        print(f"{r.status_code}")
+        assert r.status_code == 200
+
+        whats_on = ast.literal_eval(r.text)['data']['attributes']
+
+        yield whats_on, nexgendata
+
+
+def send_david_track(david_inputs):
+    for david_input in david_inputs:
+        daviddata = dict(xmltodict.parse(david_input))
+        present_track = next(filter(lambda x: x['@sequence'] == 'present', daviddata['wddxPacket']['item']))
+        woms_update = f"https://api.demo.nypr.digital/whats-on/v1/update?stream={stream}"
+
+        print("\tsending new track to woms ...", end=' ')
+        r = requests.post(woms_update, data=david_input.encode('utf-8'))
+        print(f"{r.status_code}\n")
+        assert r.status_code == 200
+
+        print("\tasserting WOMs knows what's on ...", end=' ')
+
+        r = requests.get(woms_whatson)
+        print(f"{r.status_code}")
+        assert r.status_code == 200
+
+        whats_on = ast.literal_eval(r.text)['data']['attributes']
+
+        yield whats_on, present_track
+
+
+playout = send_track(nexgen_inputs)
+
+
+print("TESTING WOMs & NEXGEN\n")
+whats_on_0, what_should_be_on_0 = next(playout)
+
+helpers.assert_and_report("title", whats_on_0['title'], what_should_be_on_0['title'])
+helpers.assert_and_report("composer", whats_on_0['mm_composer1'], what_should_be_on_0['comment1'])
+helpers.assert_and_report("ID", whats_on_0['mm_uid'], what_should_be_on_0['number'])
+
+print("\n\ttesting that woms has playlist history\n")
+
+whats_on_1, what_should_be_on_1 = next(playout)
+
+php = whats_on_1['playlist_hist_preview']
+last_track = php[0]
+helpers.assert_and_report("last track title", last_track['title'], what_should_be_on_0['title'])
+helpers.assert_and_report("last track composer", last_track['mm_composer1'], what_should_be_on_0['comment1'])
+helpers.assert_and_report("last track ID", last_track['mm_uid'], what_should_be_on_0['number'])
+print()
+helpers.assert_and_report("current track title", whats_on_1['title'], what_should_be_on_1['title'])
+helpers.assert_and_report("current track composer", whats_on_1['mm_composer1'], what_should_be_on_1['comment1'])
+helpers.assert_and_report("current track ID", whats_on_1['mm_uid'], what_should_be_on_1['number'])
+
+print("\n\nTESTING WOMs & DAViD\n")
+
+david_playout = send_david_track(david_inputs)
+
+whats_on_0, what_should_be_on_0 = next(david_playout)
+
+helpers.assert_and_report("title", whats_on_0['title'], what_should_be_on_0['Title'])
+helpers.assert_and_report("composer", whats_on_0['mm_composer1'], what_should_be_on_0['Music_Composer'])
+helpers.assert_and_report("ID", whats_on_0['mm_uid'], what_should_be_on_0['Music_MusicID'])
 
 print("\nwham, bam, thank you, ma'am!", end='\n\n')
-
-
 
